dcaa272fc974b7847faa0c7b91b97a879d374e75
diegoefraingonzal <13064@portalcedi.edu.mx>
2025-01-23 09:23:50 -0600

    Final Commit

--- quizp1/src/app/home/home.page.ts
+++ quizp1/src/app/home/home.page.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  standalone: true,
+  imports: [CommonModule, IonicModule, ReactiveFormsModule, FormsModule],
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
 })
 export class HomePage {
-  constructor() {}
+  eventForm: FormGroup;
+  events: any[] = [];
+
+  constructor() {
+    this.eventForm = new FormGroup({
+      eventName: new FormControl('', Validators.required),
+      startDate: new FormControl('', Validators.required),
+      endDate: new FormControl('', Validators.required),
+      location: new FormControl('', Validators.required),
+      managerName: new FormControl('', Validators.required),
+      managerPhone: new FormControl('', [Validators.required, Validators.pattern('^[0-9]{10}$')]),
+      isConfirmed: new FormControl(false),
+    });
+  }
+
+  saveEvent() {
+    if (this.eventForm.valid) {
+      this.events.push({...this.eventForm.value});
+      this.eventForm.reset();
+    }
+  }
+
+  confirmEvent(index: number) {
+    this.events[index].isConfirmed = !this.events[index].isConfirmed;
+  }
 }
